4f7b67ca146030f9773687f661825522992b62e4
vincent caldwell <vcaldwel8@yahoo.com>
2020-07-29 13:02:35 -0400

    adding files for github pages

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import React, {Component} from 'react';
+import './App.css';
+import {CardList} from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component.jsx"
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state={
+      monsters: [],
+      searchField:''
+    };
+    //Below not required since using arrow function below to define method
+    // this.handleChange =this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = event => {
+        this.setState({searchField: event.target.value})
+  }
+
+
+  render(){
+    const {monsters, searchField } =this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+  return (
+    <div className="App">
+      <h1> Monsters Rolodex </h1>
+      <SearchBox 
+        placeholder='search monsters'
+        handleChange= {this.handleChange}
+      />
+      <CardList monsters ={filteredMonsters}/> 
+    </div>
+  );
+  }
+}
+
+export default App;
