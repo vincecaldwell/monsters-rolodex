900a96b9db03055231a232cfa4d65d5e894f59a7
vincent caldwell <vcaldwel8@yahoo.com>
2020-07-29 13:01:42 -0400

    Updates

---
+++ precache-manifest.dde4b53f8ad351d8b160be65e94b9b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685d6dbd5e1be96c081a440943193858",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9ead21b5fe8135c1fa53",
+    "url": "/monsters-rolodex/static/css/main.db1a1201.chunk.css"
+  },
+  {
+    "revision": "59e4f92d1b90ad9a8e5b",
+    "url": "/monsters-rolodex/static/js/2.8d1ef770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8d1ef770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ead21b5fe8135c1fa53",
+    "url": "/monsters-rolodex/static/js/main.20caecbb.chunk.js"
+  },
+  {
+    "revision": "84e575cabe572d947520",
+    "url": "/monsters-rolodex/static/js/runtime-main.b47b9af8.js"
+  }
+]);
